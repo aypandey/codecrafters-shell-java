8be049f76c5cd71e3f0d6d7facd4f04af7da474f
Ayush Pandey <ayush.p@perfios.com>
2025-12-30 17:27:59 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
          System.out.print("$ ");
+
+         Scanner scanner = new Scanner(System.in);
+         String input = scanner.nextLine();
+         System.out.printf("%s: command not found", input);
+         scanner.close();
     }
 }
