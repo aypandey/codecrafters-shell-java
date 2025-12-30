7b139f46dcc4a67c7cdd949842b67c57c7a8c7c8
Ayush Pandey <ayush.p@perfios.com>
2025-12-30 17:44:44 +0530

    REPL for errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        Scanner scanner = new Scanner(System.in);
         while (true) {
          System.out.print("$ ");
-
-         Scanner scanner = new Scanner(System.in);
          String input = scanner.nextLine();
          System.out.printf("%s: command not found", input);
          System.out.println();
-         scanner.close();
-         }
+        }
     }
 }
