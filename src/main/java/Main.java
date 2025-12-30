ec4ba274c481376edaea50db84b05556c2d82d5a
Ayush Pandey <ayush.p@perfios.com>
2025-12-30 17:37:52 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         while (true) {
-            System.out.print("$ ");
+         System.out.print("$ ");
 
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            System.out.printf("%s: command not found", input);
-            scanner.close();
-        }
+         Scanner scanner = new Scanner(System.in);
+         String input = scanner.nextLine();
+         System.out.printf("%s: command not found", input);
+         System.out.println();
+         scanner.close();
+         }
     }
 }
